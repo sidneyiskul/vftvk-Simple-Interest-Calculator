396cb6470e73de6e51856813c6d922369db7f246
sidneyiskul <sidney.noah@gmail.com>
2022-05-26 04:04:41 +0000

    Done

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal < 0 || principal === "") {
+        document.getElementById("result").innerHTML="Please enter a principal postive amount.";
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years *rate / 100
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML=
+        `If you deposit <span class="highlight">${principal}</span>,<br/>
+        at an interest rate of <span class="highlight">${rate}%</span>,<br/>
+        you will receive an amount of <span class="highlight">${interest}</span>,<br/>
+        in the year <span class="highlight">${year}</span>.<br/>`; 
 }
-        
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText= `${rateval}%`;
+}
